2d7404a2d2cb7b8ce7af6e59c2f872d8d4028fa8
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-02-20 18:38:20 +0900

    0과 5로만 이러우진 정수 구하기/내 풀이및 호야님 풀이 추가

--- 0220/2.js
+++ 0220/2.js
@@ -31,3 +31,25 @@ function solution(l, r) {
     }
     return answer;
 }
+
+/*
+// 숫자 5로 현혹시켰지만 사실 이건 이진수 문제임. -> 호야님 풀이
+function* gen50() {
+    let i = 1;
+
+    while(true) {
+        yield Number(Number(i).toString(2)) * 5;
+        i++;
+    }
+}
+function solution(l, r) {
+    const n = gen50();
+    let a = 0;
+    const arr = [];
+
+    while(a < l) { a = n.next().value; }
+    while(a <= r) { arr.push(a); a = n.next().value; }
+
+    return arr.length ? arr : [-1];
+}
+ */
